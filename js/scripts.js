5a715bbe6d0848af41f14f451c0fcbd39fa7a36e
balakarthikas <73989698+balakarthikas@users.noreply.github.com>
2020-11-28 13:47:39 +0530

    update

--- js/scripts.js
+++ js/scripts.js
@@ -236,3 +236,183 @@ $(document).scroll(function() {
 $(window).load(function() {
     $("body").addClass("page-loaded");
 });
+// own
+var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+  
+    breakpoints: {
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 10
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 15
+      },
+      500: {
+        slidesPerView: 1
+      },
+    },
+  
+  
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
+window.addEventListener("load", () => {
+    var carousels = document.querySelectorAll(".carousel");
+    for (var i = 0; i < carousels.length; i++) {
+      carousel(carousels[i]);
+    }
+  });
+  
+  function carousel(root) {
+    var figure = root.querySelector("figure"),
+      nav = root.querySelector("nav"),
+      images = figure.children,
+      n = images.length,
+      gap = root.dataset.gap || 0,
+      bfc = "bfc" in root.dataset,
+      theta = (2 * Math.PI) / n,
+      currImage = 0;
+    setupCarousel(n, parseFloat(getComputedStyle(images[0]).width));
+    window.addEventListener("resize", () => {
+      setupCarousel(n, parseFloat(getComputedStyle(images[0]).width));
+    });
+  
+    setupNavigation();
+  
+    function setupCarousel(n, s) {
+      var apothem = s / (2 * Math.tan(Math.PI / n));
+      figure.style.transformOrigin = `50% 50% ${-apothem}px`;
+  
+      for (var i = 0; i < n; i++) images[i].style.padding = `${gap}px`;
+      for (i = 1; i < n; i++) {
+        images[i].style.transformOrigin = `50% 50% ${-apothem}px`;
+        images[i].style.transform = `rotateY(${i * theta}rad)`;
+      }
+      if (bfc)
+        for (i = 0; i < n; i++) images[i].style.backfaceVisibility = "hidden";
+  
+      rotateCarousel(currImage);
+    }
+  
+    function setupNavigation() {
+      nav.addEventListener("click", onClick, true);
+  
+      function onClick(e) {
+        e.stopPropagation();
+  
+        var t = e.target;
+        if (t.tagName.toUpperCase() != "BUTTON") return;
+  
+        if (t.classList.contains("next")) {
+          currImage++;
+        } else {
+          currImage--;
+        }
+  
+        rotateCarousel(currImage);
+      }
+    }
+  
+    function rotateCarousel(imageIndex) {
+      figure.style.transform = `rotateY(${imageIndex * -theta}rad)`;
+    }
+  }
+  $(document).ready(function () {
+    //rotation speed and timer
+    var speed = 5000;
+    
+    var run = setInterval(rotate, speed);
+    var slides = $('.slide');
+    var container = $('#slides ul');
+    var elm = container.find(':first-child').prop("tagName");
+    var item_width = container.width();
+    var previous = 'prev'; //id of previous button
+    var next = 'next'; //id of next button
+    slides.width(item_width); //set the slides to the correct pixel width
+    container.parent().width(item_width);
+    container.width(slides.length * item_width); //set the slides container to the correct total width
+    container.find(elm + ':first').before(container.find(elm + ':last'));
+    resetSlides();
+    
+    
+    //if user clicked on prev button
+    
+    $('#buttons a').click(function (e) {
+        //slide the item
+        
+        if (container.is(':animated')) {
+            return false;
+        }
+        if (e.target.id == previous) {
+            container.stop().animate({
+                'left': 0
+            }, 1500, function () {
+                container.find(elm + ':first').before(container.find(elm + ':last'));
+                resetSlides();
+            });
+        }
+        
+        if (e.target.id == next) {
+            container.stop().animate({
+                'left': item_width * -2
+            }, 1500, function () {
+                container.find(elm + ':last').after(container.find(elm + ':first'));
+                resetSlides();
+            });
+        }
+        
+        //cancel the link behavior            
+        return false;
+        
+    });
+    
+    //if mouse hover, pause the auto rotation, otherwise rotate it    
+    container.parent().mouseenter(function () {
+        clearInterval(run);
+    }).mouseleave(function () {
+        run = setInterval(rotate, speed);
+    });
+    
+    
+    function resetSlides() {
+        //and adjust the container so current is in the frame
+        container.css({
+            'left': -1 * item_width
+        });
+    }
+    
+});
+//a simple function to click next link
+//a timer will call this function, and the rotation will begin
+
+function rotate() {
+    $('#next').click();
+}
+var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    //direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  })
+    
+
